f1e38bec910fc8ce4e370761dd30456f7151246c
sora <chuanqi.sun.dev@gmail.com>
2022-08-19 11:48:07 -0700

    use relative path

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   clearScreen: false,
+  base: "",
   plugins: [preact()],
   server: {
     open: "/debug.html",
